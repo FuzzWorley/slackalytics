608c90da7056faccb5e865bec4a1abcf3e8fdc4b
Fuzz Worley <bitcopia@gmail.com>
2016-02-04 14:45:08 -0800

    customize for rbnd

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var qs = require('querystring');
+var slackWrapi = require('slack-wrapi');
+var fs = require('fs');
 
-//set up heroku environment variables
-var env_var = {
-	ga_key: process.env.GOOGLE_ANALYTICS_UAID
-};
+var config = JSON.parse(fs.readFileSync(config.json))
+console.log(config.auth_key + "   " + config.GA_UA)
+
+var client = new slackWrapi(config.auth_key);
 
 //Server Details
 var app = express();
@@ -18,21 +20,50 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
+//Functions
+function updateEmailList(req, res){
+	client.users.list(function(err, data) {
+	  if (!err) {
+	    var users = data;
+	    var email = {}
+	    users.members.forEach(function(member){ 
+	    		email[member.name] = member.profile.email;
+	    });
+	    fs.writeFile('user_list.json', JSON.stringify(email));
+	    console.log('User list updated.')
+	  } else {
+	  	console.log('Something is not right with me.');
+	  };
+	});
+};
+
+setInterval(updateEmailList, 360000)
 
 //Routes
 app.get('/', function(req, res){
-	res.send('here');
+	res.send('Hey! What are you doing here?');
+});
+
+app.get('/users', function(req, res){
+	updateEmailList(req, res);
+	res.send('You just manually updated the user list!')
 });
 
 app.post('/collect', function(req, res){
+  
+	var user_list = JSON.parse(fs.readFileSync('user_list.json'));
 
 	var channel = {
 		id: 	req.body.channel_id,
 		name: 	req.body.channel_name
 	};
 	var user = {
-		id: 	req.body.user_id
+		id: 	req.body.user_id,
+		name:   req.body.user_name,
 	};
+
+	user.email = user_list[user.name]
+
 	var msgText = req.body.text;
 	var teamDomain = req.body.team_domain;
 
@@ -57,26 +88,30 @@ app.post('/collect', function(req, res){
 	var wordCount = searchS(/\s+\b/);
 	var emojiCount = searchM(/:[a-z_0-9]*:/g);
 	var exclaCount = searchM(/!/g);
-	var questionMark = searchM(/\?/g);
 	var elipseCount = searchM(/\.\.\./g);
+	var questionMark = searchM(/\?/g);
 
 
 	//Structure Data
+	//cd = custom dimension
+	//cm = custom metric
+	//values line up with index in GA
 	var data = {
 		v: 		1,
-		tid: 	env_var.ga_key,
+		tid: 	config.GA_UA,
 		cid: 	user.id,
 		ds:  	"slack", //data source
 		cs: 	"slack", // campaign source
 		cd1: 	user.id,
 		cd2: 	channel.name,
-		cd3: 	msgText,
+		cd3: 	user.name+"("+user.email+")",
+		//cd4:  ,
+		cd5:  msgText,
 		cm1: 	wordCount,
 		cm2: 	emojiCount,
 		cm3: 	exclaCount,
-	//	cm4: 	letterCount,
-		cm5: 	elipseCount, 
-		cm6: 	questionMark, //need to set up in GA
+		cm4: 	elipseCount, 
+		cm5: 	questionMark,
 		dh:		teamDomain+".slack.com",
 		dp:		"/"+channel.name,
 		dt:		"Slack Channel: "+channel.name,
@@ -88,12 +123,14 @@ app.post('/collect', function(req, res){
 	};
 	console.log(JSON.stringify(data));
 	console.log(req.body);
+
 	//Make Post Request	
 	request.post("https://www.google-analytics.com/collect?" + qs.stringify(data), 
-		function(error, resp, body){
+	function(error, resp, body){
 		console.log(error);
-	})
-	res.send("OK")
+	});
+
+	res.send("OK");
 });
 
 //Start Server
